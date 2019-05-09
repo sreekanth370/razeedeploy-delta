8657815d38483bff17d899f73b49223ae7821297
Alexander Lewitt <alewitt2@ibm.com>
2019-05-09 13:17:54 -0400

    Only do aws if key and secret is defined

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 /**
-* Copyright 2019 IBM Corp. All Rights Reserved.
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+ * Copyright 2019 IBM Corp. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 const { KubeClass, KubeApiConfig } = require('@razee/kubernetes-util');
 const kubeApiConfig = KubeApiConfig();
 const kc = new KubeClass(kubeApiConfig);
@@ -72,13 +72,17 @@ async function update(uri) {
 
 async function download(uri) {
   log.debug(`Download ${uri}`);
+  let aws;
+  if (process.env.ACCESS_KEY_ID && process.env.SECRET_ACCESS_KEY) {
+    aws = {
+      key: process.env.ACCESS_KEY_ID,
+      secret: process.env.SECRET_ACCESS_KEY
+    };
+  }
   let res = await request.get({
     uri: uri,
     json: true,
-    aws: {
-      key: process.env.ACCESS_KEY_ID,
-      secret: process.env.SECRET_ACCESS_KEY
-    },
+    aws: aws,
     headers: {
       // 'If-None-Match': objectPath.get(lastModified, [uri, 'etag']),
       'If-Modified-Since': objectPath.get(lastModified, [uri, 'last-modified'])
